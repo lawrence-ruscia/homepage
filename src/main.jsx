380c518c26a294f5f0118f65880e9385bc4240fb
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-07-03 22:03:01 +0800

    style: add normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
+import '@/assets/styles/global.css';
+import 'normalize.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
