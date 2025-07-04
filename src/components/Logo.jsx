f97a45786b86865101b2639b1ecf61cbe64ef9cb
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-07-04 14:22:42 +0800

    style(header): make logo to a link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import { ChevronRight } from 'lucide-react';
-export const Logo = ({ className, iconStyles }) => {
+export const Logo = ({ eleId = '', className, iconStyles }) => {
   return (
-    <div className={className}>
+    <a href={`#${eleId}`} className={className}>
       <ChevronRight className={iconStyles} width='1.5rem' height='auto' />
       <div className='text'>Renz</div>
-    </div>
+    </a>
   );
 };
